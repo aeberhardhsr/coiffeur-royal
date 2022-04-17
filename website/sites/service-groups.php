726ed71db98cb5a9aec44012e61875cff043d2d0
aeberharhsr <19borki94@gmail.com>
2022-04-17 17:09:05 +0200

    replace add button

--- website/sites/service-groups.php
+++ website/sites/service-groups.php
@@ -243,7 +243,7 @@
 			<main class="content">
 				<div class="container-fluid p-0">
 
-					<h1 class="h3 mb-3">Dienstleistungsgruppen</h1>
+					<h1 class="h3 mb-3">Dienstleistungsgruppen <button type='button' class='btn mr-1 createservicegroupbtn'><i class='align-middle' data-feather='plus-circle' style='width: 30px; height: 30px;'></i></button></h1>
 
 					<div class="row">
 						<div class="col-12">
@@ -288,7 +288,6 @@
                                           </tbody>
                                         </table>
                                       </div>
-                                      <button class="btn btn-secondary mt-5 createservicegroupbtn">DL Gruppe hinzufügen</button>
 								</div>
 							</div>
 						</div>
