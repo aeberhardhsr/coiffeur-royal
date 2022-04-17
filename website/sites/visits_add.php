8d2c4b635ebe48412615492de682837ddc53bff9
aeberharhsr <19borki94@gmail.com>
2022-04-17 17:09:29 +0200

    edit query for update

--- website/sites/visits_add.php
+++ website/sites/visits_add.php
@@ -6,8 +6,9 @@ if(isset($_POST['createvisitbtn']))
     $visit_name      = $_POST['createVisitModal_name'];
     //$visit_date      = date("d.m.Y", strtotime("today"));
     $visit_date     = $_POST['createVisitModal_date'];
-    
-    $query = "INSERT INTO visits (visits_customer, visits_datetime) VALUES ('$visit_name', '$visit_date')";
+    $visit_assignee = $_POST['createVisitModal_assignee'];
+
+    $query = "INSERT INTO visits (visits_customer, visits_datetime, visits_assignee) VALUES ('$visit_name', CURDATE(), '$visit_assignee')";
     $query_run = mysqli_query($db_conn, $query);
 
     if($query_run)
